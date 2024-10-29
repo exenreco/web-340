f9d6a06c52572c76253fc898d0e97e3ed0a78efc
Exenreco Bell <exenreco19@yahoo.com>
2024-10-29 14:55:14 -0500

    Chapter 2 - Cooking app complete !

--- week-2/cooking-app/index.js
+++ week-2/cooking-app/index.js
@@ -1,10 +1,27 @@
 /**
- * Author:
- * Date:
- * File Name:
- * Description:
+ * Author:      Exenreco Bell
+ * Date:        October 29, 2024
+ * File Name:   index.js
+ * Description: Starter file for the recipes app.
 */
 
 // TODO: Import your module using require
 
+const { createRecipe, setTimer, quit } = require('./recipes.js');
+
 // TODO: Implement your CLI program here
+
+// Demonstrate the createRecipe function
+console.log("Demonstrating createRecipe function:");
+const recipeResult = createRecipe(['flour', 'sugar', 'butter', 'eggs']);
+console.log(recipeResult); // Expected: Recipe created with ingredients: flour, sugar, butter, eggs
+
+// Demonstrate the setTimer function
+console.log("\nDemonstrating setTimer function:");
+const timerResult = setTimer(20);
+console.log(timerResult); // Expected: Timer set for 20 minutes
+
+// Demonstrate the quit function
+console.log("\nDemonstrating quit function:");
+const quitResult = quit();
+console.log(quitResult); // Expected: Program exited
