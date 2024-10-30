b7339678e207cfd11f7ee480163f1a769ad2dd93
Exenreco Bell <exenreco19@yahoo.com>
2024-10-29 20:45:01 -0500

    Chapter 7 - Fantasy Game Character Creation Stream (Complete)!

--- week-7/fantasy-character-creation-stream/src/character-creator.js
+++ week-7/fantasy-character-creation-stream/src/character-creator.js
@@ -1,17 +1,39 @@
-const { Duplex } = require('stream');
+/**
+ * Author:        Exenreco Bell
+ * Date:          October 29, 2024
+ * File Name:     character-creator.js
+ * Description:   A class used for creating a character creation system for a fantasy video game
+ */
 
+const { Duplex } = require('stream');
 class CharacterCreator extends Duplex {
   constructor(options) {
     super(options);
     // TODO: Initialize your class here
+    this.data = [];
   }
 
   _write(chunk, encoding, callback) {
     // TODO: Implement your _write method here
+    const input = chunk.toString();
+    if (!input) {
+      this.emit('error', new Error('Empty string written to CharacterCreator'));
+    } else {
+      this.data.push(input);
+      callback();
+    }
   }
 
   _read(size) {
     // TODO: Implement your _read method here
+    const character = {
+      classType: this.data[0] || 'Unknown',
+      gender: this.data[1] || 'Unknown',
+      funFact: this.data[2] || 'No fun fact provided'
+    };
+    const description = `${character.classType} (${character.gender}): ${character.funFact}`;
+    this.push(description);
+    this.push(null); // End the stream
   }
 }
 
--- week-7/fantasy-character-creation-stream/test/character-creator.spec.js
+++ week-7/fantasy-character-creation-stream/test/character-creator.spec.js
@@ -1,3 +1,9 @@
+/**
+ * Author:        Exenreco Bell
+ * Date:          October 29, 2024
+ * File Name:     character-creator.spec.js
+ * Description:   cli test for the  character creator class
+ */
 const CharacterCreator = require('../src/character-creator');
 
 describe('CharacterCreator', () => {
@@ -9,13 +15,34 @@ describe('CharacterCreator', () => {
 
   test("should process data correctly when written to", (done) => {
     // TODO: Write your test here
+    characterCreator.write("Mage");
+    characterCreator.write("Female");
+    characterCreator.write("Loves arcane mysteries.");
+
+    characterCreator.on('data', (data) => {
+      expect(data.toString()).toBe("Mage (Female): Loves arcane mysteries.");
+      done();
+    });
   });
 
   test("should emit 'error' when invalid data is written", (done) => {
     // TODO: Write your test here
+    characterCreator.on('error', (err) => {
+      expect(err.message).toBe('Empty string written to CharacterCreator');
+      done();
+    });
+    characterCreator.write("");
   });
 
   test("should transform data correctly when written to", (done) => {
     // TODO: Write your test here
+    characterCreator.write("Warrior");
+    characterCreator.write("Male");
+    characterCreator.write("Enjoys fighting dragons.");
+
+    characterCreator.on('data', (data) => {
+      expect(data.toString()).toBe("Warrior (Male): Enjoys fighting dragons.");
+      done();
+    });
   });
 });
